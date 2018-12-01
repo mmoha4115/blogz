8532ecb3c52c9d3d2e2c33f4abe99f7ffac8b99e
Mohamed Mohamed <mmohamed54d0qf@gmail.com>
2018-12-01 16:10:26 -0600

    added address search with google streetview api, wiki opensearch for relavant links and nytimes article search api

--- static/js/script.js
+++ static/js/script.js
@@ -1,65 +1,68 @@
-
 function loadData() {
 
-    var $body = $('body');
-    var $wikiElem = $('#wikipedia-links');
-    var $nytHeaderElem = $('#nytimes-header');
-    var $nytElem = $('#nytimes-articles');
-    var $greeting = $('#greeting');
-    var $city = $('#city').val();
-    var $street = $('#street').val();
-    var address = ''+$street+','+ $city+'';
-    var nydata;
-    console.log(address);
-
-    $greeting.text('So you want to live at '+address);
-    var sig = 'BAwytdnPPWjLSzuOyDJMN37WF6c=';
-    var key = 'AIzaSyDAhXzmx1PMU7WOLfDUdTF3asXEDmBWXs0';
-    var url ='http://maps.googleapis.com/maps/api/streetview?size=600x300&location='+address+'&key=AIzaSyDAhXzmx1PMU7WOLfDUdTF3asXEDmBWXs0 ';
-    $('body').append("<img class ='bgimg' src='"+url+"'>");
-
-    console.log("should work");
-
-    // clear out old data before new request
-    $wikiElem.text("");
-    $nytElem.text("");
-
-   var url2 = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-    url2 += '?' + $.param({
-      'api-key': "f03475944ea54779981234dbda3ee235",
-      'q': address
-    });
-    console.log(url2);
-    var items = {};
-    $.ajax({
-      url: url2,
-      method: 'GET',
-    }).done(function(result) {
-      console.log(result,'retult');
-       var count=0;
-       items = result.response.docs;
-       console.log(result.response.docs,'response');
-    $.each( result.response.docs, function( key, val ) {
-        items+=( "<li id='" + key + "'>" + val.web_url + "</li>" );
-        console.log(val.web_url);
-        count++;
+  var $body = $('body');
+  var $wikiElem = $('#wikipedia-links');
+  var $nytHeaderElem = $('#nytimes-header');
+  var $nytElem = $('#nytimes-articles');
+  var $greeting = $('#greeting');
+  var $city = $('#city').val();
+  var $street = $('#street').val();
+  var address = ''+$street+','+ $city+'';
+// google street view api
+  $greeting.text('So you want to live at '+address);
+  var sig = 'BAwytdnPPWjLSzuOyDJMN37WF6c=';
+  var key = 'AIzaSyDAhXzmx1PMU7WOLfDUdTF3asXEDmBWXs0';
+  var url ='http://maps.googleapis.com/maps/api/streetview?size=600x300&location='+address+'&key=AIzaSyDAhXzmx1PMU7WOLfDUdTF3asXEDmBWXs0 ';
+  $('body').append("<img class ='bgimg' src='"+url+"'>");
+  // clear out old data before new request
+  $wikiElem.text("");
+  $nytElem.text("");
+// ny times api - article search
+ var page = 0;
+  function nytime(page){
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+  url += '?' + $.param({
+    'api-key': "f03475944ea54779981234dbda3ee235",
+    'q': address,
+    'page': page
   });
-    }).fail(function(err) {
-      throw err;
+  $.getJSON(url).done(function(result) {
+    $nytHeaderElem.text("New York articles about " +$city);
+    $.each( result.response.docs, function( key, val ) {
+    $($nytElem).append("<li id='" + 'article' + "'>" +"<a href='"+ val.web_url+"'>"+ val.headline.main + "</a><p >" + val.snippet + "</p></li>");
+    });      
+    }).fail(function(err) {    
+    $nytHeaderElem.text("There was an error and nytimes articles could not be loaded.");
+    throw err;
     });
-    console.log(items);
-  //   
-  //   var count=0;
-  //   $.each( nydata, function( key, val ) {
-  //   items.push( "<li id='" + key + "'>" + val + "</li>" );
-  //   console.log(items[count],'count');
-  //   count++;
-  //   console.log(items)
-  // });
+  }
+nytime(page);
+//  wiki search links
+function wiki(){
+  var url = "https://en.wikipedia.org/w/api.php";
+url += '?' + $.param({
+  'action': 'opensearch',
+  'format': 'json',
+  'search':$city,
+});
+var wikiErrorHandling = setTimeout(function(){
+  $wikiElem.text("Failed to ger wikipedia search");
+},10000);
+$.ajax({
+  url: url,
+  method: 'GET',
+  dataType: 'jsonp',
+}).done(function(result) {
+  var count = 0;
+  while (count < result[1].length) {
+  $($wikiElem).append("<li><a href='"+result[3][count] +"'>"+ result[1][count] + "</a></li>");
+  count++;
+  }; clearTimeout(wikiErrorHandling);    
+  });
+}
+wiki();
 
-    // YOUR CODE GOES HERE!
-
-    return false;
+return false;
 };
 
 $('#form-container').submit(loadData);
